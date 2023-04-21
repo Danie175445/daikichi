ee284c4fb90a684f99744ef43f36ed0ee505811b
Daniel Castillo <danny4004@gmail.com>
2023-04-20 17:27:59 -0700

    path variables

--- src/main/java/com/daniel/daikichi/DaikichiController.java
+++ src/main/java/com/daniel/daikichi/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.daniel.daikichi;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +21,16 @@ public class DaikichiController {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
 	
+	@RequestMapping("/daikichi/travel/{location}")
+	public String travel(@PathVariable("location") String location) {
+		return "Congratulations! You will soon travel to " + location + "!";
+	}
+	
+	@RequestMapping("/daikichi/lotto/{number}")
+	public String lotto(@PathVariable("number") int number) {
+		if(number % 2 ==  0)
+			return "You will take a grand journey in the near future, but be weary of tempting offers";
+		else
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends. ";
+	}
 }
